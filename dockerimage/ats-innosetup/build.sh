1fa5716fa2c7ccedebe2de48338411e3cc19c627
Jürg Otter <juerg.otter@gmail.com>
2025-03-21 21:57:04 +0100

    DockerImage: Update Build Scripts

--- dockerimage/ats-innosetup/build.sh
+++ dockerimage/ats-innosetup/build.sh
@@ -12,10 +12,17 @@ fi
 docker build --no-cache --platform=linux/amd64 -t ${DOCKERIMAGE}:${VERSION}-amd64 .
 docker build --no-cache --platform=linux/arm64 -t ${DOCKERIMAGE}:${VERSION}-arm64 .
 
+sleep 5
+
 docker push ${DOCKERIMAGE}:${VERSION}-amd64
 docker push ${DOCKERIMAGE}:${VERSION}-arm64
 
+sleep 5
+
 docker manifest create ${DOCKERIMAGE}:${VERSION} --amend ${DOCKERIMAGE}:${VERSION}-amd64 --amend ${DOCKERIMAGE}:${VERSION}-arm64
+sleep 5
 docker manifest push ${DOCKERIMAGE}:${VERSION}
+
+sleep 5
 
 docker buildx imagetools create -t ${DOCKERIMAGE} ${DOCKERIMAGE}:${VERSION}
