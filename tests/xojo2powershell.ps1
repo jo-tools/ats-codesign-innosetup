d35f2e555e80cde45e20eec330515ac564e991d9
Jürg Otter <juerg.otter@gmail.com>
2025-03-28 22:08:48 +0100

    Test Project: Docker Run Env

---
+++ tests/xojo2powershell.ps1
@@ -0,0 +1,17 @@
+# Get the command from the clipboard
+$command = Get-Clipboard
+
+# Check if the clipboard is not empty
+if (![string]::IsNullOrWhiteSpace($command)) {
+    try {
+        # Execute the command and capture the output
+        $output = Invoke-Expression $command
+        
+        # Output the result
+        Write-Output $output
+    } catch {
+        Write-Host "Error executing command: $_"
+    }
+} else {
+    Write-Host "Clipboard is empty or does not contain a valid command."
+}
